f1124a0df180d01ea98bbbfbb98d845b00a655d6
nubis <valerio.panebianco.04@gmail.com>
2024-03-27 20:54:25 +0100

    First Commit

---
+++ src/main/java/org/nubis/brick/game/Main.java
@@ -0,0 +1,25 @@
+package org.nubis.brick.game;
+
+import javax.swing.*;
+
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //1. Create the frame.
+        JFrame frame = new JFrame("FrameDemo");
+
+        //2. Optional: What happens when the frame closes?
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //3. Create components and put them in the frame.
+        //...create emptyLabel...
+
+        //4. Size the frame.
+        frame.pack();
+
+        //5. Show it.
+        frame.setVisible(true);
+    }
+}
