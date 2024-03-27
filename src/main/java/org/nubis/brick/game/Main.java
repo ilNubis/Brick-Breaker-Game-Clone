a8a1d8e000b6b91229c6d622af8f1c25e5980057
nubis <valerio.panebianco.04@gmail.com>
2024-03-28 00:36:26 +0100

    General Upgrade

--- src/main/java/org/nubis/brick/game/Main.java
+++ src/main/java/org/nubis/brick/game/Main.java
@@ -1,25 +1,37 @@
 package org.nubis.brick.game;
 
 import javax.swing.*;
-
+import java.awt.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Main{
     public static void main(String[] args) {
         //1. Create the frame.
-        JFrame frame = new JFrame("FrameDemo");
+        GameView mainWindow = new GameView();
 
-        //2. Optional: What happens when the frame closes?
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //3. Create components and put them in the frame.
-        //...create emptyLabel...
+        long SEC_NSEC = 1000000000;
+        int MAX_FPS = 60;
+        long TIME_SINGLE_FRAME = (SEC_NSEC/MAX_FPS);
 
-        //4. Size the frame.
-        frame.pack();
+        long execTimeFrame = 1;
+        long startTime = 0;
+        long endTime = 0;
 
-        //5. Show it.
-        frame.setVisible(true);
+        // prendere gli fps, capire se sono maggiori di quelli massimi, non eseguire
+        while (true){
+            if (execTimeFrame >= TIME_SINGLE_FRAME || startTime == 0) {
+                startTime = System.nanoTime();
+                // ... Code
+                mainWindow.update();
+                endTime = System.nanoTime();
+            } else{
+                endTime = System.nanoTime();
+            }
+            execTimeFrame = (endTime-startTime); // tempo in singolo
+
+        }
+
     }
 }
